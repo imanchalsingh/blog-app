e21bd434f01278df939aef855f278195eff80e76
anchalDev159357 <singhanchal159357@gmail.com>
2025-01-20 18:17:05 +0530

    added changes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,3 @@
-// App.tsx
 import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
@@ -7,26 +6,27 @@ import MyPosts from "./Components/Blog App/my post/MyPosts";
 import Home from "./Components/Blog App/Home";
 import Login from "./Components/Auth/Login";
 import Register from "./Components/Auth/Register";
-import { AuthProvider } from "./Components/Auth/AuthContext"; // Import AuthProvider
+import { AuthProvider } from "./Components/Auth/AuthContext";
 import Archive from "./Components/Blog App/Archive";
-import Articles from "./Components/Blog App/Articles.tsx";
+import Articles from "./Components/Blog App/Articles";
+import LikedPost from "./Components/Blog App/LikedPost";
 
 const App: React.FC = () => {
   return (
     <AuthProvider>
       {" "}
-      {/* Wrap everything with AuthProvider */}
+      {/* Wrap everything with LikedPostProvider */}
       <Router>
         <Routes>
           <Route path="/" element={<Dashboard />}>
             <Route index element={<Home />} />
             <Route path="/myposts" element={<MyPosts />} />
             <Route path="/archive" element={<Archive />} />
-            <Route path="articles" element={<Articles />} />
-            <Route path="settings" element={<div>Settings Page</div>} />
+            <Route path="/articles" element={<Articles />} />
+            <Route path="/likedpost" element={<LikedPost />} />
           </Route>
-          <Route path="/login" element={<Login />}></Route>
-          <Route path="/register" element={<Register />}></Route>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
           <Route path="/dashboard" element={<Dashboard />}>
             <Route index element={<Home />} />
           </Route>
